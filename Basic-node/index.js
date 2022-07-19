2b80ea4dc512cfd4ec07c3d02a4ffaf070872a29
Swapnil Tiwari <swaptiwari@outlook.com>
2022-07-20 01:03:08 +0530

    Basic Node

---
+++ Basic-node/index.js
@@ -0,0 +1,7 @@
+// const fs = require("fs")
+const sh = require("superheroes");
+const sv = require("supervillains")
+
+// copyFileSync("file1.txt", "file2.txt");
+console.log(sh.random());
+console.log(sv.random());

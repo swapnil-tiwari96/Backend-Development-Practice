835b25fe826d56adffcef773d46f01e67e2884b7
Swapnil Tiwari <swaptiwari@outlook.com>
2022-08-06 08:21:00 +0530

    npm init

--- codeCampTutorial/Node/app.js
+++ codeCampTutorial/Node/app.js
@@ -1,15 +1,20 @@
-const os = require("os");
+// npm - global command, comes with node
+// npm --version
 
-// info about the user
-console.log(os.userInfo());
+// local dependency - use it only in this directory
+// npm i <packagename>
 
-// method returns the uptime in seconds
-console.log(`The system uptime is ${os.uptime()} seconds.`);
+// global dependency - use it in any project
+// npm i -g <packagename>
+// sudo npm i -g <packagename>
 
-const currentOS = {
-    name: os.type(),
-    release: os.release(),
-    totalMemory: os.totalmem(),
-    freeMemory: os.freemem()
-}
-console.log(currentOS);
+// package.json - manifest file (stores important info about project/package)
+// manual approach - (create package.json in the root, create properties etc)
+// npm init (step by step - press enter to skip)
+// npm init -y (everything default)
+
+const _ = require('lodash'); //This will only happen if I installed the package already.
+
+const items = [1, [2, [3, [4]]]];
+const newItems = _.flattenDeep(items);
+console.log(newItems);
